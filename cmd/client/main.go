623b280c3b0bb5d754096abc90aa61d1ca7ef57f
Victor Ribeiro <hyukodev@gmail.com>
2025-10-20 20:41:11 +0200

    update client main.go to create user-specific queues

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/hyuko21/pubsub-golang/internal/gamelogic"
+	"github.com/hyuko21/pubsub-golang/internal/pubsub"
+	"github.com/hyuko21/pubsub-golang/internal/routing"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -25,7 +28,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error welcoming new user: %s", err)
 	}
-	log.Printf("New user connected: %s", username)
+	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
+	_, q, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.TransientQueue)
+	if err != nil {
+		log.Fatalf("Error declaring queue: %s", err)
+	}
+	log.Printf("Created new queue for user: '%s' %v\n", q.Name, q)
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
